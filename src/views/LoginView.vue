7e98aee5ae1c731a8c978e98c3f258d3e1b99cc9
MahdiRoshan <dev.mahdi@hotmail.com>
2023-05-10 16:20:53 +0330

    composable for autoComplete input created

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -14,17 +14,6 @@
     <main>
         <h1>{{ $t('Login') }}</h1>
         <AutoCompleteInput label="search name" placeholder="search in names ..." id="search name" :data="names"/>
+        <DatePicker />
     </main>
 </template>
-<script setup>
-import AutoCompleteInput from "./../components/AutoComplete.vue";
-
-    const names = [
-        {name:"mahdi" , id:Math.floor(Math.random() * 999)+1},
-        {name:"reza" , id:Math.floor(Math.random() * 999)+1},
-        {name:"ali" , id:Math.floor(Math.random() * 999)+1},
-        {name:"mohammad" , id:Math.floor(Math.random() * 999)+1}
-    ]
-
-
-</script>
