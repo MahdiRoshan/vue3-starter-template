10d7bc705770f0a40574674e64695289561c712a
MahdiRoshan <dev.mahdi@hotmail.com>
2023-05-17 16:34:03 +0330

    update auto complete

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,4 @@
 <template>
     <RouterView />
-    <AutoCompleteInput  :data="data" bind-value="title" v-model="value"/>
-<!--    <AutoCompleteInput  :data="data1"  v-model="value"/>-->
 </template>
-<script setup>
-import AutoCompleteInput from "./components/AutoComplete.vue";
-import {ref} from "vue";
-const value= ref([]);
-const data = [{id:1 , title:"mahdi"} , {id:2 , title:"reza"} , {id:3 , title:"ali"} , {id:4 , title:"majid"}];
-</script>
+
